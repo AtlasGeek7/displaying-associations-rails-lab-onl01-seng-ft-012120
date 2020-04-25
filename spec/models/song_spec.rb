0260c2d97ac4c4a14c6e63ede50b1e2e713ed7e6
Khalid T <khalid.tissire@gmail.com>
2020-04-25 12:18:06 +0000

    Automatically backed up by Learn

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -7,8 +7,5 @@ RSpec.describe Song, type: :model do
     @artist = Artist.create!(name: "Daft Punk")
     @grid = @artist.songs.create!(title: "The Grid")
   end
-
-  it "gets the artist name" do
-    expect(@grid.artist_name).to eq("Daft Punk")
-  end
 end
+
