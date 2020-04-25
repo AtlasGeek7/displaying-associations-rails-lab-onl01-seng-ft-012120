652fcef02a8d2437b16a2c9bc43e2e483770079f
Khalid T <khalid.tissire@gmail.com>
2020-04-25 12:18:26 +0000

    Automatically backed up by Learn

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,11 +1,2 @@
 require 'rails_helper'
 
-RSpec.describe Song, type: :model do
-  before do
-    Artist.destroy_all
-    Song.destroy_all
-    @artist = Artist.create!(name: "Daft Punk")
-    @grid = @artist.songs.create!(title: "The Grid")
-  end
-end
-
